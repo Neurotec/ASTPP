15dd6526efcaf7263ff3033c873e92a8d2127aaf
Neurotec Tecnologia S.A.S <dirindesa@neurotec.co>
2016-08-15 16:58:16 -0500

    Update cron.php
    rename Lowcredit to LowCredit

--- web_interface/astpp/application/config/cron.php
+++ web_interface/astpp/application/config/cron.php
@@ -10,7 +10,7 @@ $astpp_config = parse_ini_file("/var/lib/astpp/astpp-config.conf");
 $config['SERVER_NAME'] 		= $astpp_config['base_url'];	// Your web site url
 $config['CRON_TIME_LIMIT']	= 0;								// 0 = no time limit
 $config['argv']			= array("LowBalance"=>"lowbalance/low_balance",
-					"Lowcredit"=>"lowcreditlimit/low_creditlimit",
+					"LowCredit"=>"lowcreditlimit/low_creditlimit",
 					"UpdateBalance" => "updateBalance/GetUpdateBalance",
 				        "CurrencyUpdate" => "currencyupdate/update_currency",
 					"GenerateInvoice" => "generateInvoice/getInvoiceData",
